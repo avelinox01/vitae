76e102685287353f8e86a898f7f06dde0ceba88a
avelinox01 <101229630+avelinox01@users.noreply.github.com>
2024-04-09 16:38:07 -0300

    link whatsapp

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -8,7 +8,7 @@ module.exports = {
                 </a>`,
     'X': `<a href="#e"><i class="fa-brands fa-twitter fact-icon"></i>Avelino</a>`,
     'Email': '<a href="mailto:avelinox01@gmail.com"><i class="fa fa-envelope fact-icon"></i>avelinox01@gmail.com</a>',
-    'WhatSapp': '<a href="mailto:avelinox01@gmail.com"><i class="fa-brands fa-square-whatsapp fact-icon"></i> (62) 99271-4963</a>'
+    'WhatSapp': '<a href="https://wa.me/5562992714963"><i class="fa-brands fa-square-whatsapp fact-icon"></i> Avelino </a>'
   },
   // skills: [
   //   ['PHP', 100],
